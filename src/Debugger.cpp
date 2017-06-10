85b1efd44f209f239900f546fc4d71bfbedb6807
Antonio Maiorano <amaiorano@gmail.com>
2017-06-09 23:32:50 -0400

    Disassemble indexed addressing mode instructions

--- src/Debugger.cpp
+++ src/Debugger.cpp
@@ -37,10 +37,50 @@ namespace {
         return result;
     }
 
+    const char* GetRegisterName(const CpuRegisters& cpuRegisters, const uint8_t& r) {
+        ptrdiff_t offset =
+            reinterpret_cast<const uint8_t*>(&r) - reinterpret_cast<const uint8_t*>(&cpuRegisters);
+        switch (offset) {
+        case offsetof(CpuRegisters, A):
+            return "A";
+        case offsetof(CpuRegisters, B):
+            return "B";
+        case offsetof(CpuRegisters, DP):
+            return "DP";
+        case offsetof(CpuRegisters, CC):
+            return "CC";
+        default:
+            assert(false);
+            return "INVALID";
+        }
+    };
+
+    const char* GetRegisterName(const CpuRegisters& cpuRegisters, const uint16_t& r) {
+        ptrdiff_t offset =
+            reinterpret_cast<const uint8_t*>(&r) - reinterpret_cast<const uint8_t*>(&cpuRegisters);
+        switch (offset) {
+        case offsetof(CpuRegisters, X):
+            return "X";
+        case offsetof(CpuRegisters, Y):
+            return "Y";
+        case offsetof(CpuRegisters, U):
+            return "U";
+        case offsetof(CpuRegisters, S):
+            return "S";
+        case offsetof(CpuRegisters, PC):
+            return "PC";
+        case offsetof(CpuRegisters, D):
+            return "D";
+        default:
+            assert(false);
+            return "INVALID";
+        }
+    };
+
     struct Instruction {
         const CpuOp& cpuOp;
         int page;
-        std::array<uint8_t, 2> operands;
+        std::array<uint8_t, 3> operands;
     };
 
     Instruction ReadInstruction(uint16_t opAddr, const MemoryBus& memoryBus) {
@@ -55,17 +95,189 @@ namespace {
         }
 
         const auto& cpuOp = LookupCpuOpRuntime(cpuOpPage, opCodeByte);
-        const int numOperands = cpuOp.size - 1 - (cpuOpPage == 0 ? 0 : 1);
-
         Instruction result = {cpuOp, cpuOpPage};
-        for (int i = 0; i < numOperands; ++i) {
-            result.operands[i] = memoryBus.Read(opAddr++);
+
+        // Always read max operand bytes, even if they're not used. We would want to only read as
+        // many operands as stipulated by the opcode's size - 1 (or 2 if not on page 0), but
+        // unfortunately, indexed instructions sometimes read an extra operand byte, determined
+        // dynamically. So for disassembling purposes, we just read 3 bytes and call it a day.
+        for (auto& operand : result.operands) {
+            operand = memoryBus.Read(opAddr++);
         }
         return result;
     }
 
-    std::string DisassembleOp(const CpuRegisters& reg, const MemoryBus& memoryBus) {
-        uint16_t opAddr = reg.PC;
+    void DisassembleIndexedInstruction(const Instruction& instruction,
+                                       const CpuRegisters& cpuRegisters, const MemoryBus& memoryBus,
+                                       std::string& disasmInstruction, std::string& comment) {
+        auto RegisterSelect = [&cpuRegisters](uint8_t postbyte) -> const uint16_t& {
+            switch ((postbyte >> 5) & 0b11) {
+            case 0b00:
+                return cpuRegisters.X;
+            case 0b01:
+                return cpuRegisters.Y;
+            case 0b10:
+                return cpuRegisters.U;
+            default: // 0b11:
+                return cpuRegisters.S;
+            }
+        };
+
+        uint16_t EA = 0;
+        uint8_t postbyte = instruction.operands[0];
+        bool supportsIndirect = true;
+        std::string operands;
+
+        if ((postbyte & BITS(7)) == 0) // (+/- 4 bit offset),R
+        {
+            // postbyte is a 5 bit two's complement number we convert to 8 bit.
+            // So if bit 4 is set (sign bit), we extend the sign bit by turning on bits 6,7,8;
+            uint8_t offset = postbyte & 0b0000'1111;
+            if (postbyte & BITS(4))
+                offset |= 0b1110'0000;
+            auto& reg = RegisterSelect(postbyte);
+            EA = reg + S16(offset);
+            supportsIndirect = false;
+
+            operands = FormattedString<>("%d,%s", offset, GetRegisterName(cpuRegisters, reg));
+            comment = FormattedString<>("%d,$%04x", offset, reg);
+        } else {
+            switch (postbyte & 0b1111) {
+            case 0b0000: { // ,R+
+                auto reg = RegisterSelect(postbyte);
+                EA = reg;
+                reg += 1;
+                supportsIndirect = false;
+
+                operands = FormattedString<>(",%s+", GetRegisterName(cpuRegisters, reg));
+                comment = FormattedString<>(",$%04x", reg);
+            } break;
+            case 0b0001: { // ,R++
+                auto reg = RegisterSelect(postbyte);
+                EA = reg;
+                reg += 2;
+
+                operands = FormattedString<>(",%s++", GetRegisterName(cpuRegisters, reg));
+                comment = FormattedString<>(",$%04x", reg);
+            } break;
+            case 0b0010: { // ,-R
+                auto reg = RegisterSelect(postbyte);
+                reg -= 1;
+                EA = reg;
+                supportsIndirect = false;
+
+                operands = FormattedString<>(",-%s", GetRegisterName(cpuRegisters, reg));
+                comment = FormattedString<>(",$%04x", reg);
+            } break;
+            case 0b0011: { // ,--R
+                auto reg = RegisterSelect(postbyte);
+                reg -= 2;
+                EA = reg;
+
+                operands = FormattedString<>(",--%s", GetRegisterName(cpuRegisters, reg));
+                comment = FormattedString<>(",$%04x", reg);
+            } break;
+            case 0b0100: { // ,R
+                auto& reg = RegisterSelect(postbyte);
+                EA = reg;
+
+                operands = FormattedString<>(",%s", GetRegisterName(cpuRegisters, reg));
+                comment = FormattedString<>(",$%04x", reg);
+            } break;
+            case 0b0101: { // (+/- B),R
+                auto& reg = RegisterSelect(postbyte);
+                auto offset = S16(cpuRegisters.B);
+                EA = reg + offset;
+
+                operands = FormattedString<>("B,%s", GetRegisterName(cpuRegisters, reg));
+                comment = FormattedString<>("%d,$%04x", offset, reg);
+            } break;
+            case 0b0110: { // (+/- A),R
+                auto& reg = RegisterSelect(postbyte);
+                auto offset = S16(cpuRegisters.A);
+                EA = reg + offset;
+
+                operands = FormattedString<>("A,%s", GetRegisterName(cpuRegisters, reg));
+                comment = FormattedString<>("%d,$%04x", offset, reg);
+            } break;
+            case 0b0111:
+                FAIL("Illegal");
+                break;
+            case 0b1000: { // (+/- 7 bit offset),R
+                auto& reg = RegisterSelect(postbyte);
+                uint8_t postbyte2 = instruction.operands[2];
+                auto offset = S16(postbyte2);
+                EA = reg + offset;
+
+                operands = FormattedString<>("%d,%s", offset, GetRegisterName(cpuRegisters, reg));
+                comment = FormattedString<>("%d,$%04x", offset, reg);
+            } break;
+            case 0b1001: { // (+/- 15 bit offset),R
+                uint8_t postbyte2 = instruction.operands[2];
+                uint8_t postbyte3 = instruction.operands[3];
+                auto& reg = RegisterSelect(postbyte);
+                auto offset = CombineToS16(postbyte2, postbyte3);
+                EA = reg + offset;
+
+                operands = FormattedString<>("%d,%s", offset, GetRegisterName(cpuRegisters, reg));
+                comment = FormattedString<>("%d,$%04x", offset, reg);
+            } break;
+            case 0b1010:
+                FAIL("Illegal");
+                break;
+            case 0b1011: { // (+/- D),R
+                auto& reg = RegisterSelect(postbyte);
+                auto offset = S16(cpuRegisters.D);
+                EA = reg + offset;
+
+                operands = FormattedString<>("D,%s", GetRegisterName(cpuRegisters, reg));
+                comment = FormattedString<>("%d,$%04x", offset, reg);
+            } break;
+            case 0b1100: { // (+/- 7 bit offset),PC
+                uint8_t postbyte2 = instruction.operands[2];
+                auto offset = S16(postbyte2);
+                EA = cpuRegisters.PC + offset;
+
+                operands = FormattedString<>("%d,PC", offset);
+                comment = FormattedString<>("%d,$%04x", offset, cpuRegisters.PC);
+            } break;
+            case 0b1101: { // (+/- 15 bit offset),PC
+                uint8_t postbyte2 = instruction.operands[2];
+                uint8_t postbyte3 = instruction.operands[3];
+                auto offset = CombineToS16(postbyte2, postbyte3);
+                EA = cpuRegisters.PC + offset;
+
+                operands = FormattedString<>("%d,PC", offset);
+                comment = FormattedString<>("%d,$%04x", offset, cpuRegisters.PC);
+            } break;
+            case 0b1110:
+                FAIL("Illegal");
+                break;
+            case 0b1111: { // [address] (Indirect-only)
+                uint8_t postbyte2 = instruction.operands[2];
+                uint8_t postbyte3 = instruction.operands[3];
+                EA = CombineToS16(postbyte2, postbyte3);
+            } break;
+            default:
+                FAIL("Illegal");
+                break;
+            }
+        }
+
+        if (supportsIndirect && (postbyte & BITS(4))) {
+            uint8_t msb = memoryBus.Read(EA);
+            uint8_t lsb = memoryBus.Read(EA + 1);
+            EA = CombineToU16(lsb, msb);
+            operands = "[" + operands + "]";
+        }
+
+        disasmInstruction = std::string(instruction.cpuOp.name) + " " + operands;
+        uint8_t value = memoryBus.Read(EA);
+        comment += FormattedString<>(" = $%04x = $%02x (%d)", EA, value, value);
+    }
+
+    std::string DisassembleOp(const CpuRegisters& cpuRegisters, const MemoryBus& memoryBus) {
+        uint16_t opAddr = cpuRegisters.PC;
         auto instruction = ReadInstruction(opAddr, memoryBus);
         const auto& cpuOp = instruction.cpuOp;
 
@@ -119,19 +331,20 @@ namespace {
         } break;
 
         case AddressingMode::Direct: {
-            uint16_t EA = CombineToU16(reg.DP, instruction.operands[0]);
+            uint16_t EA = CombineToU16(cpuRegisters.DP, instruction.operands[0]);
             uint8_t value = memoryBus.Read(EA);
             disasmInstruction = FormattedString<>("%s $%02x", cpuOp.name, instruction.operands[0]);
             comment = FormattedString<>("DP:(PC) = $%02x = $%02x (%d)", EA, value, value);
         } break;
 
-        case AddressingMode::Indexed: { //@TODO
-            disasmInstruction = cpuOp.name;
+        case AddressingMode::Indexed: {
+            DisassembleIndexedInstruction(instruction, cpuRegisters, memoryBus, disasmInstruction,
+                                          comment);
         } break;
 
         case AddressingMode::Relative: {
             // Branch instruction with 8 or 16 bit signed relative offset
-            auto nextPC = reg.PC + cpuOp.size;
+            auto nextPC = cpuRegisters.PC + cpuOp.size;
             if (cpuOp.size == 2) {
                 auto offset = static_cast<int8_t>(instruction.operands[0]);
                 disasmInstruction = FormattedString<>("%s $%02x", cpuOp.name, U16(offset) & 0x00FF);
@@ -157,23 +370,24 @@ namespace {
         return result;
     };
 
-    void PrintOp(const CpuRegisters& reg, const MemoryBus& memoryBus) {
-        std::string op = DisassembleOp(reg, memoryBus);
-        std::cout << FormattedString<>("[$%x] %s", reg.PC, op.c_str()) << std::endl;
+    void PrintOp(const CpuRegisters& cpuRegisters, const MemoryBus& memoryBus) {
+        std::string op = DisassembleOp(cpuRegisters, memoryBus);
+        std::cout << FormattedString<>("[$%x] %s", cpuRegisters.PC, op.c_str()) << std::endl;
     }
 
-    void PrintRegisters(const CpuRegisters& reg) {
-        const auto& cc = reg.CC;
+    void PrintRegisters(const CpuRegisters& cpuRegisters) {
+        const auto& cc = cpuRegisters.CC;
 
         std::string CC = FormattedString<>(
             "%c%c%c%c%c%c%c%c", cc.Carry ? 'C' : 'c', cc.Overflow ? 'V' : 'v', cc.Zero ? 'Z' : 'z',
             cc.Negative ? 'N' : 'n', cc.InterruptMask ? 'I' : 'i', cc.HalfCarry ? 'H' : 'h',
             cc.FastInterruptMask ? 'F' : 'f', cc.Entire ? 'E' : 'e');
 
+        const auto& r = cpuRegisters;
         std::cout << FormattedString<>("A=$%02x (%d) B=$%02x (%d) D=$%04x (%d) X=$%04x (%d) "
                                        "Y=$%04x (%d) U=$%04x S=$%04x DP=$%02x PC=$%04x CC=%s",
-                                       reg.A, reg.A, reg.B, reg.B, reg.D, reg.D, reg.X, reg.X,
-                                       reg.Y, reg.Y, reg.U, reg.S, reg.DP, reg.PC, CC.c_str())
+                                       r.A, r.A, r.B, r.B, r.D, r.D, r.X, r.X, r.Y, r.Y, r.U, r.S,
+                                       r.DP, r.PC, CC.c_str())
                   << std::endl;
     }
 
