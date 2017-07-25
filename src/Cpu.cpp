7d3b1693c1ef95620052cfa694508c8875a2d635
Antonio Maiorano <amaiorano@gmail.com>
2017-07-24 23:12:35 -0400

    Reorder branch instructions by op code value

--- src/Cpu.cpp
+++ src/Cpu.cpp
@@ -993,53 +993,53 @@ public:
                 OpLBSR();
                 break;
 
-            case 0x24: // BCC (branch if carry clear) or BHS (branch if higher or same)
-                OpBranch([this] { return CC.Carry == 0; });
-                break;
-            case 0x25: // BCS (branch if carry set) or BLO (branch if lower)
-                OpBranch([this] { return CC.Carry != 0; });
-                break;
-            case 0x27: // BEQ (branch if equal)
-                OpBranch([this] { return CC.Zero != 0; });
-                break;
-            case 0x2C: // BGE (branch if greater or equal)
-                OpBranch([this] { return (CC.Negative ^ CC.Overflow) == 0; });
-                break;
-            case 0x2E: // BGT (branch if greater)
-                OpBranch([this] { return (CC.Zero | (CC.Negative ^ CC.Overflow)) == 0; });
-                break;
-            case 0x22: // BHI (branch if higher)
-                OpBranch([this] { return (CC.Carry | CC.Zero) == 0; });
-                break;
-            case 0x2F: // BLE (branch if less or equal)
-                OpBranch([this] { return (CC.Zero | (CC.Negative ^ CC.Overflow)) != 0; });
-                break;
-            case 0x23: // BLS (banch if lower or same)
-                OpBranch([this] { return (CC.Carry | CC.Zero) != 0; });
-                break;
-            case 0x2D: // BLT (branch if less than)
-                OpBranch([this] { return (CC.Negative ^ CC.Overflow) != 0; });
-                break;
-            case 0x2B: // BMI (brach if minus)
-                OpBranch([this] { return CC.Negative != 0; });
-                break;
-            case 0x26: // BNE (branch if not equal)
-                OpBranch([this] { return CC.Zero == 0; });
-                break;
-            case 0x2A: // BPL (branch if plus)
-                OpBranch([this] { return CC.Negative == 0; });
-                break;
             case 0x20: // BRA (branch always)
                 OpBranch([this] { return true; });
                 break;
             case 0x21: // BRN (branch never)
                 OpBranch([this] { return false; });
                 break;
+            case 0x22: // BHI (branch if higher)
+                OpBranch([this] { return (CC.Carry | CC.Zero) == 0; });
+                break;
+            case 0x23: // BLS (banch if lower or same)
+                OpBranch([this] { return (CC.Carry | CC.Zero) != 0; });
+                break;
+            case 0x24: // BCC (branch if carry clear) or BHS (branch if higher or same)
+                OpBranch([this] { return CC.Carry == 0; });
+                break;
+            case 0x25: // BCS (branch if carry set) or BLO (branch if lower)
+                OpBranch([this] { return CC.Carry != 0; });
+                break;
+            case 0x26: // BNE (branch if not equal)
+                OpBranch([this] { return CC.Zero == 0; });
+                break;
+            case 0x27: // BEQ (branch if equal)
+                OpBranch([this] { return CC.Zero != 0; });
+                break;
             case 0x28: // BVC (branch if overflow clear)
                 OpBranch([this] { return CC.Overflow == 0; });
                 break;
             case 0x29: // BVS (branch if overflow set)
                 OpBranch([this] { return CC.Overflow != 0; });
+                break;
+            case 0x2A: // BPL (branch if plus)
+                OpBranch([this] { return CC.Negative == 0; });
+                break;
+            case 0x2B: // BMI (brach if minus)
+                OpBranch([this] { return CC.Negative != 0; });
+                break;
+            case 0x2C: // BGE (branch if greater or equal)
+                OpBranch([this] { return (CC.Negative ^ CC.Overflow) == 0; });
+                break;
+            case 0x2D: // BLT (branch if less than)
+                OpBranch([this] { return (CC.Negative ^ CC.Overflow) != 0; });
+                break;
+            case 0x2E: // BGT (branch if greater)
+                OpBranch([this] { return (CC.Zero | (CC.Negative ^ CC.Overflow)) == 0; });
+                break;
+            case 0x2F: // BLE (branch if less or equal)
+                OpBranch([this] { return (CC.Zero | (CC.Negative ^ CC.Overflow)) != 0; });
                 break;
 
             case 0x1E: // EXG (exchange/swap register values)
