0eb6d5e6ac9a43e0c0a4f5882861bf49b9b8989f
Antonio Maiorano <amaiorano@gmail.com>
2017-07-27 16:21:38 -0400

    Improve MemoryBus performance by using a sorted vector instead of a map

--- src/MemoryBus.h
+++ src/MemoryBus.h
@@ -1,7 +1,8 @@
 #pragma once
 
 #include "Base.h"
-#include <map>
+#include <algorithm>
+#include <vector>
 
 using MemoryRange = std::pair<uint16_t, uint16_t>;
 
@@ -13,7 +14,12 @@ struct IMemoryBusDevice {
 class MemoryBus {
 public:
     void ConnectDevice(IMemoryBusDevice& device, MemoryRange range) {
-        m_map.emplace(std::make_pair(range, DeviceInfo{&device, range}));
+        m_devices.push_back(DeviceInfo{&device, range});
+
+        std::sort(m_devices.begin(), m_devices.end(),
+                  [](const DeviceInfo& info1, const DeviceInfo& info2) {
+                      return info1.memoryRange.first < info2.memoryRange.first;
+                  });
     }
 
     uint8_t Read(uint16_t address) const { return FindDeviceInfo(address).device->Read(address); }
@@ -24,20 +30,26 @@ public:
 
 private:
     struct DeviceInfo {
-        IMemoryBusDevice* device;
+        IMemoryBusDevice* device = nullptr;
         MemoryRange memoryRange;
     };
 
     const DeviceInfo& FindDeviceInfo(uint16_t address) const {
-        for (auto& entry : m_map) {
-            if (address >= entry.first.first && address <= entry.first.second)
-                return entry.second;
+        // We assume at least 1 device is connected. This one condition allows us to check the
+        // address against the end of each range in the inner loop.
+        if (address >= m_devices[0].memoryRange.first) {
+            for (const auto& info : m_devices) {
+                if (address <= info.memoryRange.second) {
+                    return info;
+                }
+            }
         }
+
         assert(false && "Unmapped address");
         static DeviceInfo nullDeviceInfo{};
         return nullDeviceInfo;
     }
 
-    //@TODO: replace with vector of DeviceInfo and just use linear searches
-    std::map<MemoryRange, DeviceInfo> m_map;
+    // Sorted by first address in range
+    std::vector<DeviceInfo> m_devices;
 };
