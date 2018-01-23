6407a1ccad4308977292d463cbe2b54434ed11fb
Antonio Maiorano <amaiorano@gmail.com>
2018-01-22 22:10:23 -0500

    Sync frame start (with frame time and input) and frame end for server and client

---
+++ src/SyncProtocol.h
@@ -0,0 +1,77 @@
+#pragma once
+
+#include "ConsoleOutput.h"
+#include "EngineClient.h"
+#include "Tcp.h"
+#include <chrono>
+#include <memory>
+#include <thread>
+
+namespace SyncMsg {
+
+    enum class Type {
+        FrameStart,
+        FrameEnd,
+    };
+
+    struct FrameStart {
+        double frameTime{};
+        Input input{};
+    };
+
+} // namespace SyncMsg
+
+class SyncProtocol {
+public:
+    void InitServer() {
+        m_server = std::make_unique<TcpServer>();
+        Errorf("Server: about to accept connection...\n");
+        m_server->Open(9123);
+        while (!m_server->TryAccept()) {
+            Errorf("Server: no connection, retrying...\n");
+            using namespace std::chrono_literals;
+            std::this_thread::sleep_for(10ms);
+        }
+        Errorf("Server: Connected!\n");
+    }
+
+    void InitClient() {
+        m_client = std::make_unique<TcpClient>();
+        Errorf("Client: about to connect...\n");
+        m_client->Open("127.0.0.1", 9123);
+        Errorf("Client: Connected!\n");
+    }
+
+    bool IsServer() const { return m_server != nullptr; }
+    bool IsClient() const { return m_client != nullptr; }
+
+    void Server_SendFrameStart(double frameTime, const Input& input) {
+        auto message = SyncMsg::FrameStart{frameTime, input};
+        m_server->Send(SyncMsg::Type::FrameStart);
+        m_server->Send(message);
+    }
+
+    void Client_RecvFrameStart(double& frameTime, Input& input) {
+        RecvType(m_client, SyncMsg::Type::FrameStart);
+        SyncMsg::FrameStart message;
+        m_client->Receive(message);
+
+        frameTime = message.frameTime;
+        input = message.input;
+    }
+
+    void Client_SendFrameEnd() { m_client->Send(SyncMsg::Type::FrameEnd); }
+
+    void Server_RecvFrameEnd() { RecvType(m_server, SyncMsg::Type::FrameEnd); }
+
+private:
+    template <typename T>
+    void RecvType(const T& connection, SyncMsg::Type expectedType) {
+        SyncMsg::Type type;
+        connection->Receive(type);
+        ASSERT(type == expectedType);
+    }
+
+    std::unique_ptr<TcpServer> m_server;
+    std::unique_ptr<TcpClient> m_client;
+};
