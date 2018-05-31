35dd36dd8a408b4de1a18107e6ff0e9d435a2f48
Antonio Maiorano <amaiorano@gmail.com>
2018-05-31 08:56:20 -0400

    travis: add version.txt and remove deployment

--- package/package_linux.sh
+++ package/package_linux.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 # Script creates a "vectrexy" folder root directory, populates it, and zips it as vectrexy.zip
 
 script_path="$( cd "$(dirname "$0")" ; pwd -P )"
@@ -15,6 +14,8 @@ cp $root_dir/bios_rom.bin $output_dir
 cp $root_dir/README.md $output_dir
 cp $root_dir/COMPAT.md $output_dir
 cp $root_dir/LICENSE.txt $output_dir
+
+git describe > $output_dir/version.txt
 
 curl -sS $data_zip_url > data.zip
 unzip data.zip -d $output_dir
