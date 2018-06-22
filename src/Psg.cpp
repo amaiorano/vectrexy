46bc0005a818d4adfa8b3be0ff887e701fd3c93a
Antonio Maiorano <amaiorano@gmail.com>
2018-06-21 21:20:38 -0400

    Move SquareWaveGenerator to header file

--- src/Psg.cpp
+++ src/Psg.cpp
@@ -14,51 +14,7 @@ namespace {
     }
 } // namespace
 
-struct SquareWaveGenerator {
-    uint32_t m_period{}; // 12 bit value [0,4095]
-    uint32_t m_duty{};   // Period / 2 (number of clocks before oscilating)
-    uint32_t m_time{};   // Time in period
-    uint16_t m_value{};  // 0 or 1
-
-    const uint32_t PeriodScale = 16; // Psg hardware always counts min 16
-
-    void SetPeriodHigh(uint8_t high) {
-        assert(high <= 0xf); // Only 4 bits should be set
-        m_period |= high << 16;
-        UpdateDuty();
-    }
-    void SetPeriodLow(uint8_t low) {
-        m_period |= low;
-        UpdateDuty();
-    }
-
-    uint8_t PeriodHigh() const {
-        return checked_static_cast<uint8_t>(m_period >> 16); // Top 12 bits
-    }
-    uint8_t PeriodLow() const { return checked_static_cast<uint8_t>(m_period & 0xff); }
-
-    void Clock() {
-        if (--m_time == 0) {
-            m_value = m_value == 0 ? 1 : 0;
-            m_time = m_duty;
-        }
-    }
-
-    uint16_t Value() const { m_value; }
-
-private:
-    void UpdateDuty() {
-        m_duty = (std::max<uint32_t>(1, m_period) * PeriodScale) / 2;
-        assert(m_duty > 0);
-        // Should we reset time?
-    }
-};
-
 void Psg::Init() {
-    for (auto& swg : m_squareWaveGenerators) {
-        swg.reset(new SquareWaveGenerator{});
-    }
-
     Reset();
 }
 
@@ -74,6 +30,9 @@ void Psg::Reset() {
     m_mode = {};
     m_DA = {};
     m_registers.fill(0);
+    for (auto& swg : m_squareWaveGenerators) {
+        swg = {};
+    }
 }
 
 void Psg::Update(cycles_t cycles) {
@@ -114,24 +73,24 @@ void Psg::Clock() {
     }
 
     for (auto& swg : m_squareWaveGenerators) {
-        swg->Clock();
+        swg.Clock();
     }
 }
 
 uint8_t Psg::Read(uint16_t address) {
     switch (m_latchedAddress) {
     case Register::ChannelAHigh:
-        return m_squareWaveGenerators[0]->PeriodHigh();
+        return m_squareWaveGenerators[0].PeriodHigh();
     case Register::ChannelALow:
-        return m_squareWaveGenerators[0]->PeriodLow();
+        return m_squareWaveGenerators[0].PeriodLow();
     case Register::ChannelBHigh:
-        return m_squareWaveGenerators[1]->PeriodHigh();
+        return m_squareWaveGenerators[1].PeriodHigh();
     case Register::ChannelBLow:
-        return m_squareWaveGenerators[1]->PeriodLow();
+        return m_squareWaveGenerators[1].PeriodLow();
     case Register::ChannelCHigh:
-        return m_squareWaveGenerators[2]->PeriodHigh();
+        return m_squareWaveGenerators[2].PeriodHigh();
     case Register::ChannelCLow:
-        return m_squareWaveGenerators[2]->PeriodLow();
+        return m_squareWaveGenerators[2].PeriodLow();
     }
 
     return m_registers[address];
@@ -140,17 +99,17 @@ uint8_t Psg::Read(uint16_t address) {
 void Psg::Write(uint16_t address, uint8_t value) {
     switch (m_latchedAddress) {
     case Register::ChannelAHigh:
-        return m_squareWaveGenerators[0]->SetPeriodHigh(value);
+        return m_squareWaveGenerators[0].SetPeriodHigh(value);
     case Register::ChannelALow:
-        return m_squareWaveGenerators[0]->SetPeriodLow(value);
+        return m_squareWaveGenerators[0].SetPeriodLow(value);
     case Register::ChannelBHigh:
-        return m_squareWaveGenerators[1]->SetPeriodHigh(value);
+        return m_squareWaveGenerators[1].SetPeriodHigh(value);
     case Register::ChannelBLow:
-        return m_squareWaveGenerators[1]->SetPeriodLow(value);
+        return m_squareWaveGenerators[1].SetPeriodLow(value);
     case Register::ChannelCHigh:
-        return m_squareWaveGenerators[2]->SetPeriodHigh(value);
+        return m_squareWaveGenerators[2].SetPeriodHigh(value);
     case Register::ChannelCLow:
-        return m_squareWaveGenerators[2]->SetPeriodLow(value);
+        return m_squareWaveGenerators[2].SetPeriodLow(value);
     }
 
     m_registers[address] = value;
--- src/Psg.h
+++ src/Psg.h
@@ -4,6 +4,46 @@
 #include <array>
 #include <memory>
 
+struct SquareWaveGenerator {
+    uint32_t m_period{}; // 12 bit value [0,4095]
+    uint32_t m_duty{};   // Period / 2 (number of clocks before oscilating)
+    uint32_t m_time{};   // Time in period
+    uint16_t m_value{};  // 0 or 1
+
+    static const uint32_t PeriodScale = 16; // Psg hardware always counts min 16
+
+    void SetPeriodHigh(uint8_t high) {
+        assert(high <= 0xf); // Only 4 bits should be set
+        m_period |= high << 16;
+        UpdateDuty();
+    }
+    void SetPeriodLow(uint8_t low) {
+        m_period |= low;
+        UpdateDuty();
+    }
+
+    uint8_t PeriodHigh() const {
+        return checked_static_cast<uint8_t>(m_period >> 16); // Top 12 bits
+    }
+    uint8_t PeriodLow() const { return checked_static_cast<uint8_t>(m_period & 0xff); }
+
+    void Clock() {
+        if (--m_time == 0) {
+            m_value = m_value == 0 ? 1 : 0;
+            m_time = m_duty;
+        }
+    }
+
+    uint16_t Value() const { m_value; }
+
+private:
+    void UpdateDuty() {
+        m_duty = (std::max<uint32_t>(1, m_period) * PeriodScale) / 2;
+        assert(m_duty > 0);
+        // Should we reset time?
+    }
+};
+
 // Implementation of the AY-3-8912 Programmable Sound Generator (PSG)
 
 class Psg {
@@ -27,7 +67,7 @@ private:
     uint8_t Read(uint16_t address);
     void Write(uint16_t address, uint8_t value);
 
-    enum class Channel { A, B, C, NumTypes };
+    enum class Channel { A, B, C };
 
     enum class PsgMode {
         // Selected from BDIR (bit 1) and BC1 (bit 0) values
@@ -44,6 +84,5 @@ private:
     uint8_t m_DA{}; // Data/Address bus (DA7-DA0)
     uint8_t m_latchedAddress{};
     std::array<uint8_t, 16> m_registers;
-
-    std::unique_ptr<struct SquareWaveGenerator> m_squareWaveGenerators[(size_t)Channel::NumTypes];
+    std::array<SquareWaveGenerator, 3> m_squareWaveGenerators;
 };
