4b44892389b1b61900f48ce0b5974a71a8356088
Antonio Maiorano <amaiorano@gmail.com>
2018-06-29 10:33:49 -0400

    Fix and comment CircularBuffer
    * Rename PopBack to PopFront for multiple elements, since that's what we actually mean, and fix its implementation
    * Add comments on functions to clarify usage

--- src/CircularBuffer.h
+++ src/CircularBuffer.h
@@ -16,6 +16,7 @@ public:
         Clear();
     }
 
+    // Clears all values in the buffer such that UsedSize()==0, and FreeSize()==TotalSize()
     void Clear() {
         m_begin = m_buffer.empty() ? nullptr : &m_buffer.front();
         m_front = m_back = m_begin;
@@ -23,22 +24,26 @@ public:
         m_wrapped = 0;
     }
 
+    // Total number of elements that can be added to the buffer
     size_t TotalSize() const { return m_buffer.size(); }
 
+    // Number of elements in the buffer
     size_t UsedSize() const {
         if (m_wrapped == 0) // front is behind back
         {
             return m_back - m_front;
-        } else // front is ahead of back
-        {
+        } else { // front is ahead of back
             return (m_end - m_front) + (m_back - m_begin);
         }
     }
 
+    // Number of elements that can be added to the buffer before it's full
     size_t FreeSize() const { return TotalSize() - UsedSize(); }
 
+    // Returns true if there are no elements in the buffer
     bool Empty() const { return UsedSize() == 0; }
 
+    // Returns true if the buffer is full
     bool Full() const { return FreeSize() == 0; }
 
     // Attempts to push numValues from source into buffer; will not go past the front pointer.
@@ -91,6 +96,8 @@ public:
         return numValuesActuallyWritten;
     }
 
+    // Push a single element to the back of the buffer.
+    // Returns number of values actually pushed (0 or 1).
     size_t PushBack(const T& value) { return PushBack(&value, 1); }
 
     // Pushes back numValues, removing values from front if full
@@ -107,94 +114,26 @@ public:
         assert(numActuallyPushed + numExtraPushed == numValues);
     }
 
+    // Pushes single element to the back of the buffer, removing a value from the front if full.
     void PushBackMoveFront(T& value) { PushBackMoveFront(&value, 1); }
 
     // Attempts to pop numValues worth of data from the buffer into dest.
     // Returns how many values actually popped
-    size_t PopBack(T* dest, size_t numValues) {
-        assert(false && "This function doesn't work properly! Fix it.");
-
-        assert(m_wrapped < 2);
+    size_t PopFront(T* dest, size_t numValues) {
+        //@TODO: Implement optimal version without constantly calling PopFront
         size_t numValuesActuallyRead = 0;
-
-        if (m_wrapped == 1) // front >= back
-        {
-            assert(m_front >= m_back);
-            const size_t roomLeft = m_end - m_front;
-            const size_t numValuesToRead = std::min(numValues, roomLeft);
-
-            std::copy_n(m_front, numValuesToRead, dest);
-            numValuesActuallyRead += numValuesToRead;
-            m_front += numValuesToRead;
-            assert(m_front <= m_end);
-
-            if (m_front == m_end) {
-                m_front = m_begin;
-                --m_wrapped;
-            }
-
-            // If we've read all we have to, bail; otherwise, we still have more to read, so
-            // update our inputs and fall through into m_wrapped == 0 condition below
-            if (numValuesToRead == numValues) {
-                return numValuesActuallyRead;
+        for (size_t i = 0; i < numValues; ++i) {
+            if (PopFront(dest[i])) {
+                ++numValuesActuallyRead;
             } else {
-                dest += numValuesToRead;
-                numValues -= numValuesToRead;
+                break;
             }
         }
-
-        // Note: NOT "else if" here on purpose
-        if (m_wrapped == 0) // front <= back
-        {
-            assert(m_front <= m_back);
-
-            // Read as much as we can; but we can't go past the back pointer
-            size_t roomLeft = m_back - m_front;
-            const size_t numValuesToRead = std::min(numValues, roomLeft);
-            std::copy_n(m_front, numValuesToRead, dest);
-            numValuesActuallyRead += numValuesToRead;
-            m_front += numValuesToRead;
-            assert(m_front <= m_back);
-        }
-
         return numValuesActuallyRead;
     }
 
-    size_t PopBack(T& value) {
-        // Decrement back, then pop back
-
-        if (m_wrapped == 0) // front <= back
-        {
-            assert(m_front <= m_back);
-            if (m_back == m_front)
-                return 0;
-            DecBack();
-            value = *m_back;
-            return 1;
-        } else { // front >= back
-            assert(m_front >= m_back);
-            DecBack();
-            value = *m_back;
-            return 1;
-        }
-    }
-
-    size_t PeekBack(T* dest, size_t numValues) {
-        //@TODO: Implement peekback without popping and pushing
-        size_t i = 0;
-        for (; i < numValues; ++i) {
-            if (PopBack(dest[i]) == 0)
-                break;
-        }
-
-        std::reverse(dest, dest + i);
-
-        PushBack(dest, i);
-        return i;
-    }
-
-    size_t PeekBack(T& value) { return PeekBack(&value, 1); }
-
+    // Pops a single value from the front of the buffer.
+    // Returns how many values actually popped (0 or 1).
     size_t PopFront(T& value) {
         // Pop front, then increment front
 
@@ -214,6 +153,47 @@ public:
             return 1;
         }
     }
+
+    // Pops a single value from the back of the buffer.
+    // Returns how many values actually popped (0 or 1).
+    size_t PopBack(T& value) {
+        // Decrement back, then pop back
+        if (m_wrapped == 0) // front <= back
+        {
+            assert(m_front <= m_back);
+            if (m_back == m_front)
+                return 0;
+            DecBack();
+            value = *m_back;
+            return 1;
+        } else { // front >= back
+            assert(m_front >= m_back);
+            DecBack();
+            value = *m_back;
+            return 1;
+        }
+    }
+
+    // Copies up to numValues worth of values from the back of the queue in "forward" order without
+    // affecting the buffer. You must ensure that dest is at least as large as numValues. Returns
+    // how many values actually copied in dest.
+    size_t PeekBack(T* dest, size_t numValues) {
+        //@TODO: Implement peekback without popping and pushing
+        size_t i = 0;
+        for (; i < numValues; ++i) {
+            if (PopBack(dest[i]) == 0)
+                break;
+        }
+
+        std::reverse(dest, dest + i);
+
+        PushBack(dest, i);
+        return i;
+    }
+
+    // Copies a single element from the back into 'value' if possible.
+    // Returns number of values actually copied (0 or 1).
+    size_t PeekBack(T& value) { return PeekBack(&value, 1); }
 
 private:
     void IncBack() {
